1f99cb998c73efd9fe0200cdd2e9f8f1b8693a79
mffonua <108596346+mffonua@users.noreply.github.com>
2022-12-08 17:35:03 -0700

    created component folder

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <section>
+            <h1> Hello, I'm Miles Fonua </h1>
+            <article>
+                <p>
+                    I am currently enrolled at the U of U Full Stack Coding Bootcamp. This bootcamp is helping me build and refine my JavaScrip, HTML5 and CSS skills that I have learned already through my own studies.
+                </p>
+                <p>
+                    Thanks for stopping by and visiting my current Portolio! If you would like to reach out to me, please visit the Contact Section.
+                </p>
+            </article>
+        </section>
+    );
+}
+
+export default About;
